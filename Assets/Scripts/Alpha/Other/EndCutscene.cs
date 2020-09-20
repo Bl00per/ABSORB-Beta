14295da985f5f98bb924fde575ec6b18f90bd364
Uriah Overton <uri.overton@gmail.com>
2020-09-21 09:17:55 +1000

    Push first project

---
+++ Assets/Scripts/Alpha/Other/EndCutscene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class EndCutscene : MonoBehaviour
+{
+    public GameObject player;
+    public CinemachineVirtualCamera cutsceneCamera;
+
+    private BoxCollider _triggerBox;
+    private PlayerHandler _playerHandler;
+    private LocomotionHandler _locomotionHandler;
+    private CombatHandler _combatHandler;
+    private CinemachineFreeLook _mouseCamera;
+    private CinemachineFreeLook _controllerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _triggerBox = GetComponent<BoxCollider>();
+        _mouseCamera = GameObject.Find("(Mouse)Third Person Camera").GetComponent<CinemachineFreeLook>();
+        _controllerCamera = GameObject.Find("(Controller)Third Person Camera").GetComponent<CinemachineFreeLook>();
+        _playerHandler = player.GetComponent<PlayerHandler>();
+        _locomotionHandler = _playerHandler.GetLocomotionHandler();
+        _combatHandler = _playerHandler.GetCombatHandler();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            PlayEndCutscene();
+    }
+
+    // Remove all control and play cutscene
+    private void PlayEndCutscene()
+    {
+        // Play animation?
+        _mouseCamera.Priority = 0;
+        cutsceneCamera.Priority = 1;
+        _playerHandler.enabled = false;
+        _combatHandler.enabled = false;
+        _locomotionHandler.enabled = false;
+    }
+}
