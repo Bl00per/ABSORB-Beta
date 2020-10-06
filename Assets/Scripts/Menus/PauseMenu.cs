a1782ebb50a658b912ec48b822c3f09b1ae604ac
Will Hart <clouduniversemc@gmail.com>
2020-10-06 11:42:21 +1100

    Added more volume sliders and adjusted scripts to read them
    Need to work on finding all the little sounds effects that aren't assigned an output though

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -9,7 +9,11 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject settingsMenu;
-    public Slider volumeSlider;
+    [Header("Settings Menu References")]
+    public AudioMixer audioMixer; // Master, Music, SFX
+    public Slider masterVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
     public Toggle toggleButton;
     [Header("BUTTONS")]
     public GameObject pauseFirstSelectedButton;
@@ -21,10 +25,7 @@ public class PauseMenu : MonoBehaviour
     private CameraManager _cameraManager;
     private bool Paused;
     private MainMenu _mainMenu;
-    private float _masterVolume = 0f;
-    public AudioMixer _audioMixer; // Master, Music, SFX
-    [SerializeField]
-    private Image volumeFill = null;
+    private Image _masterSliderFill = null, _musicSliderFill = null, _sfxSliderFill = null;
 
     // Start is called before the first frame update
     void Start()
@@ -35,8 +36,14 @@ public class PauseMenu : MonoBehaviour
         _inputManager = FindObjectOfType<InputManager>();
         _cameraManager = FindObjectOfType<CameraManager>();
         _mainMenu = GetComponent<MainMenu>();
-        volumeSlider.value = readWrite.volume;
-        toggleButton.isOn = readWrite.overrideControls;
+
+        SetSettingsOnStart();
+
+        // This is so I don't have to assign every single fill
+        // The fact you can't even change the highlight to be the fill instead of the handle is the bane of my existence
+        _masterSliderFill = masterVolumeSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+        _musicSliderFill = musicVolumeSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+        _sfxSliderFill = sfxVolumeSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -55,24 +62,27 @@ public class PauseMenu : MonoBehaviour
 
         if (Paused)
         {
-            if (EventSystem.current.currentSelectedGameObject == volumeSlider.gameObject)
+            
+            if (settingsMenu.activeInHierarchy)
             {
-                volumeFill.color = new Vector4(213f, 0, 217f, 255f);
-            }
-            else
-            {
-                volumeFill.color = Color.white;
-            }
+                GameObject temp = EventSystem.current.currentSelectedGameObject;
+                // I can't use a switch statement for this, I really tried. Forgive me
+                if (temp == masterVolumeSlider.gameObject)
+                    _masterSliderFill.color = new Vector4(213f, 0, 217f, 255f);
+                else
+                    _masterSliderFill.color = Color.white;
 
-            if (volumeSlider.value == volumeSlider.minValue)
-                _audioMixer.SetFloat("Master", -80f); // Mute the volume
-            else
-            {
-                _masterVolume = volumeSlider.value;
-                _audioMixer.SetFloat("Master", _masterVolume); // Set the volume of the master
+                if (temp == musicVolumeSlider.gameObject)
+                    _musicSliderFill.color = new Vector4(213f, 0, 217f, 255f);
+                else
+                    _musicSliderFill.color = Color.white;
+
+                if (temp == sfxVolumeSlider.gameObject)
+                    _sfxSliderFill.color = new Vector4(213f, 0, 217f, 255f);
+                else
+                    _sfxSliderFill.color = Color.white;
             }
         }
-
     }
 
     public void Pause()
@@ -123,9 +133,7 @@ public class PauseMenu : MonoBehaviour
     // Close the settings menu and write the changes to the file despite if a change was made or not
     public void CloseSettingsMenu()
     {
-        readWrite.volume = volumeSlider.value;
-        readWrite.overrideControls = toggleButton.isOn;
-        readWrite.OverwriteData();
+        SaveSettings();
         pauseMenu.SetActive(true);
         settingsMenu.SetActive(false);
         EventSystem.current.SetSelectedGameObject(null);
@@ -135,5 +143,60 @@ public class PauseMenu : MonoBehaviour
     public void ToggleOverrideControls()
     {
         _inputManager.SetOverrideController(!_inputManager.GetOverrideController());
+    }
+
+    // Set all the settings for the game on start from the read data file (if it exists)
+    private void SetSettingsOnStart()
+    {
+        /* Volume Sliders */
+        masterVolumeSlider.value = readWrite.masterVolume;  // Master
+        audioMixer.SetFloat("Master", masterVolumeSlider.value);
+
+        musicVolumeSlider.value = readWrite.musicVolume;    // Music
+        audioMixer.SetFloat("Music", musicVolumeSlider.value);
+
+        sfxVolumeSlider.value = readWrite.sfxVolume;        // SFX
+        audioMixer.SetFloat("SFX", sfxVolumeSlider.value);
+
+        toggleButton.isOn = readWrite.overrideControls;     // Toggle override button
+    }
+
+    // Save all the settings values upon exiting the settings menu
+    private void SaveSettings()
+    {
+        readWrite.masterVolume = masterVolumeSlider.value;
+        readWrite.musicVolume = musicVolumeSlider.value;
+        readWrite.sfxVolume = sfxVolumeSlider.value;
+        readWrite.overrideControls = toggleButton.isOn;
+        readWrite.OverwriteData();
+    }
+
+    // Set the volume of the master throught the slider
+    public void SetMasterLvl(float level)
+    {
+        if (masterVolumeSlider.value == masterVolumeSlider.minValue)
+            audioMixer.SetFloat("Master", -80f);    // Mute the volume 
+        else
+            audioMixer.SetFloat("Master", level);
+    }
+
+    // Set the volume of the Music throught the slider
+    public void SetMusicLvl(float level)
+    {
+        // Mute the volume 
+        if (musicVolumeSlider.value == musicVolumeSlider.minValue)
+            audioMixer.SetFloat("Music", -80f);    // Mute the volume 
+        else
+            audioMixer.SetFloat("Music", level);
+    }
+
+    // Set the volume of the Effects throught the slider
+    public void SetSFXLvl(float level)
+    {
+        // Mute the volume 
+        if (sfxVolumeSlider.value == sfxVolumeSlider.minValue)
+            audioMixer.SetFloat("SFX", -80f);    // Mute the volume 
+        else
+            audioMixer.SetFloat("SFX", level);
     }
 }
