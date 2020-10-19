1ae83d03be810e5c6ed87fc9842b961f23b57477
Uriah Overton <uri.overton@gmail.com>
2020-10-20 10:45:01 +1100

    new animator controllers and barrel

--- Assets/Scripts/Other/Barrels.cs
+++ Assets/Scripts/Other/Barrels.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Barrels : MonoBehaviour
 {
-
+    private Collider _collider;
     private AudioSource _audio;
     private void Start()
     {
         _audio = GetComponent<AudioSource>();
+        _collider = GetComponent<Collider>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         _audio.pitch = Random.Range(0.6f, 0.9f);
         _audio.PlayOneShot(_audio.clip);
+        StartCoroutine(BarrelCD());
     }
 
-
+    public IEnumerator BarrelCD()
+    {
+        _collider.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        _collider.enabled = true;
+    }
 }
