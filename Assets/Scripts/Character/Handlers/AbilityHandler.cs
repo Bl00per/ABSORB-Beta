dd386653f340a42825014909f2c8fd9f0035e4c4
Lachlan Wernert <wernert97@live.com>
2020-10-20 16:28:16 +1100

    Heal from absorbing intractable

--- Assets/Scripts/Character/Handlers/AbilityHandler.cs
+++ Assets/Scripts/Character/Handlers/AbilityHandler.cs
@@ -346,6 +346,7 @@ public class AbilityHandler : MonoBehaviour
                     _animator.SetBool("Absorb", true);
                     _playerHandler.GetLocomotionHandler().Key_ActivateSlowdown();
                     absorbInteractable.Activate();
+                    _combatHandler.HealOvertime(healthFromAbsorb, healthFromAbosrbDuration);
                 }
                 return null;
             }
