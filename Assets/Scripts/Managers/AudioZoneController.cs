8f043ae2cbfcfa89a7d0b15d5a3773bd2c25ddd0
Will Hart <clouduniversemc@gmail.com>
2020-11-02 14:33:00 +1100

    Input manager now runs on Timer class instead of Coroutine
    Cleaned up a bunch of scripts too

--- Assets/Scripts/Managers/AudioZoneController.cs
+++ Assets/Scripts/Managers/AudioZoneController.cs
@@ -1,23 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AudioZoneController : MonoBehaviour
 {
-
     public AudioSource AmbientWind;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
--- Assets/Scripts/Other/SimpleCameraShakeInCinemachine.cs
+++ Assets/Scripts/Other/SimpleCameraShakeInCinemachine.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using UnityEngine.Events;
 
 public class SimpleCameraShakeInCinemachine : MonoBehaviour
 {
