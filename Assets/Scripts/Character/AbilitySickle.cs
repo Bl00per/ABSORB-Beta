a40a05743b3505ca6685a0b2b1e863ccbc238bcb
Lachlan Wernert <wernert97@live.com>
2020-11-10 14:46:12 +1100

    Ability Sickle plays in world space again

--- Assets/Scripts/Character/AbilitySickle.cs
+++ Assets/Scripts/Character/AbilitySickle.cs
@@ -9,18 +9,27 @@ public class AbilitySickle : Ability
     public Collider sickleCollider;
     public AudioSource sickleSFX;
 
+    [Header("Properties")]
+    public float vfxTime = 1.5f;
+
     private PlayerHandler _playerHandler;
     private Animator _animator;
+    private Transform _parent;
+    private Vector3 _initPos;
+    private Quaternion _initRot;
 
     public void Awake()
     {
         _typeOfAbility = AbilityHandler.AbilityType.SICKLE;
         _playerHandler = this.GetComponent<PlayerHandler>();
+        _parent = sickleGameObject.transform.parent;
     }
 
     public void Start()
     {
         _animator = _playerHandler.GetAnimator();
+        _initPos = sickleGameObject.transform.localPosition;
+        _initRot = sickleGameObject.transform.localRotation;
     }
 
     public override void OnEnter() { }
@@ -30,20 +39,21 @@ public class AbilitySickle : Ability
     public override void Activate()
     {
         _animator.SetBool("Sickle", true);
-        //sickleGameObject.SetActive(true);
     }
+
     public void Key_ActivateSickleAbility()
     {
-
         sickleGameObject.SetActive(true);
-
     }
+
     public void Key_DeactivateSickleAbility()
     {
         _animator.SetBool("Sickle", false);
-        sickleGameObject.SetActive(false);
+        sickleGameObject.transform.SetParent(null);
         abilityHandler.SetAbility(AbilityHandler.AbilityType.NONE);
+        StartCoroutine(ReparentVFX());
     }
+
     public void Key_sickleSFX()
     {
          sickleSFX.Play();
@@ -59,5 +69,12 @@ public class AbilitySickle : Ability
         sickleCollider.enabled = false;
     }
 
-
+    private IEnumerator ReparentVFX()
+    {
+        yield return new WaitForSeconds(vfxTime);
+        sickleGameObject.transform.SetParent(_parent);
+        sickleGameObject.transform.localPosition = _initPos;
+        sickleGameObject.transform.localRotation = _initRot;
+        sickleGameObject.SetActive(false);
+    }
 }
